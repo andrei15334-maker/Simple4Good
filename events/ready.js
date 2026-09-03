40915dacaec495df87f61823c228b808a4f0de73
andrei15334-maker <andrei15334@gmail.com>
2026-09-03 18:25:49 +0300

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -7,9 +7,23 @@ const modalsSystem = require('../systems/modals');
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`[BOT] Conectat ca ${client.user.tag}`);
         client.user.setActivity('FiveM S4G', { type: 0 }); // 0 = Playing
+
+        // Paraseste serverul specificat daca botul se afla pe el
+        try {
+            const targetGuildId = '1521363170982232186';
+            const targetGuild = client.guilds.cache.get(targetGuildId);
+            if (targetGuild) {
+                await targetGuild.leave();
+                console.log(`[SUCCES] Botul a părăsit serverul: ${targetGuild.name} (${targetGuildId})`);
+            } else {
+                console.log(`[INFO] Botul nu se află pe serverul cu ID-ul ${targetGuildId}`);
+            }
+        } catch (err) {
+            console.error('[EROARE] Nu am putut părăsi serverul:', err);
+        }
 
         // Initializam toate sistemele (modular)
         verifySystem.init(client);
